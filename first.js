d642b682812ef92780ac03da990aa285866c916b
shreyash <shre240101020@iiitmanipur.ac.in>
2025-07-27 15:02:29 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,114 @@
+document.getElementById("box1").addEventListener("click", () => {
+    document.body.style.backgroundColor = "red";
+    document.querySelector(".boxes").style.backgroundColor = "red";
+
+})
+
+document.getElementById("box2").addEventListener('click', () => {
+    document.querySelector("body").style.backgroundColor = "yellow";
+        document.querySelector(".boxes").style.backgroundColor = "yellow";
+
+})
+
+document.getElementById("box3").addEventListener('click', () => {
+    document.querySelector("body").style.backgroundColor = "green";
+        document.querySelector(".boxes").style.backgroundColor = "green";
+
+})
+
+document.getElementById("box4").addEventListener('click', () => {
+    document.querySelector("body").style.backgroundColor = "pink";
+        document.querySelector(".boxes").style.backgroundColor = "pink";
+
+})
+
+document.getElementById("box5").addEventListener('click', () => {
+    document.querySelector("body").style.backgroundColor = "skyblue";
+        document.querySelector(".boxes").style.backgroundColor = "skyblue";
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
